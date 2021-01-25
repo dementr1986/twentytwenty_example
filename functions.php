cd12cc2f1aada1e0fd390b56bc7ad40ff82c25f8
dementr1986 <77992065+dementr1986@users.noreply.github.com>
2021-01-26 00:00:42 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -805,15 +805,31 @@ function true_option_settings() {
 	register_setting( 'true_options', 'true_options', 'true_validate_settings' ); // true_options
  
 	// Добавляем секцию
-	add_settings_section( 'true_section_1', 'Текстовые поля ввода', '', $true_page );
+	add_settings_section( 'true_section_1', 'Text input', '', $true_page );
  
 	// Создадим текстовое поле в первой секции
 	$true_field_params = array(
 		'type'      => 'text', // тип
 		'id'        => 'my_text',
-		'desc'      => 'Пример обычного текстового поля.', // описание
+		'desc'      => 'Example.', // описание
 		'label_for' => 'my_text' // позволяет сделать название настройки лейблом (если не понимаете, что это, можете не использовать), по идее должно быть одинаковым с параметром id
 	);
-	add_settings_field( 'my_text_field', 'Текстовое поле', 'true_option_display_settings', $true_page, 'true_section_1', $true_field_params );
+	add_settings_field( 'my_text_field', 'Text input', 'true_option_display_settings', $true_page, 'true_section_1', $true_field_params );
+ add_action( 'admin_init', 'true_option_settings' );
  
+ function true_option_display_settings($args) {
+	extract( $args );
+ 
+	$option_name = 'true_options';
+ 
+	$o = get_option( $option_name );
+ 
+	switch ( $type ) {  
+		case 'text':  
+			$o[$id] = esc_attr( stripslashes($o[$id]) );
+			echo "<input class='regular-text' type='text' id='$id' name='" . $option_name . "[$id]' value='$o[$id]' />";  
+			echo ($desc != '') ? "<br /><span class='description'>$desc</span>" : "";  
+		break;
+	}
+}
 
